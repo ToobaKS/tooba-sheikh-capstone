8193f1669d0aa1703fcafdfc6a1acd4a87660309
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-15 18:46:09 -0400

    added index, route and controller files for the users

---
+++ habit-tracker-backend/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import warehouseRoutes from "./routes/warehouse-routes.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/", warehouseRoutes);
+app.use("/", inventoryRoutes);
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
