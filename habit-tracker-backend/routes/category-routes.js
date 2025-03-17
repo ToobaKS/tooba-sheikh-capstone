0794df7c3a84fcc5ff2518fd53815b0f9bba6124
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-16 22:52:16 -0400

    added the category routes and controllers

--- habit-tracker-backend/routes/category-routes.js
+++ habit-tracker-backend/routes/category-routes.js
@@ -3,6 +3,7 @@ import * as categoryController from "../controllers/category-controller.js";
 
 const router = express.Router();
 
-router.route("/categories").get(categoryController.getAllCategories);
+router.get("/", categoryController.getAllCategories); // Get all categories
+router.get("/:category_name", categoryController.getCategoryByName); // Get category by name
 
 export default router;
