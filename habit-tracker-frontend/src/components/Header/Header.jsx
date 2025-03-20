01f27125284f03181d39460fe5eba127e066d46a
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-19 23:41:19 -0400

    set up the header with some basic styling and jsx

--- habit-tracker-frontend/src/components/Header/Header.jsx
+++ habit-tracker-frontend/src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logos/logo-4.png";
 import "./Header.scss";
 
 function Header() {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <header className="header">
+      <Link to="/" className="header__logo">
+        <img src={logo} alt="Rootinely Logo" className="header__logo-img" />
+        <h2 className="header__logo-text">Rootinely</h2>
+      </Link>
+      <nav className="header__nav">
+        <NavLink to="/home" className="header__link">
+          Habits
+        </NavLink>
+        <NavLink to="/time-capsule" className="header__link">
+          Time Capsule
+        </NavLink>
+        <NavLink to="/chatbot" className="header__link">
+          Chatbot
+        </NavLink>
+        <NavLink to="/data" className="header__link">
+          Data
+        </NavLink>
+      </nav>
+    </header>
   );
 }
 
