81f8485ead82db9f75eff077b5deb538dd6d149e
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-22 00:59:16 -0400

    finished building and styling the register page

--- habit-tracker-frontend/src/components/RegisterForm/RegisterForm.jsx
+++ habit-tracker-frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -2,11 +2,37 @@ import "./RegisterForm.scss";
 
 function RegisterForm() {
   return (
-    <>
-      <div>
-        <p>© 2024 Rootinely</p>
-      </div>
-    </>
+    <form className="register-form">
+      <input
+        type="text"
+        placeholder="First Name"
+        className="register-form__input"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        className="register-form__input"
+      />
+      <input
+        type="text"
+        placeholder="Username"
+        className="register-form__input"
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        className="register-form__input"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="register-form__input"
+      />
+      <button type="submit" className="register-form__button">
+        Register
+      </button>
+    </form>
   );
 }
 
